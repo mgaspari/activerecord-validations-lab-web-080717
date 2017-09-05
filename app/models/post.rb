b5aecb333c9ce4545e9d7d4c550ca0b33db1cc27
mgaspari <marcoanthonygaspari@gmail.com>
2017-09-05 13:44:00 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { :minimum => 250 }
+  validates :summary, length: { :maximum => 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate  :validateCB
+  # validates :title, inclusion: { in: %w( "Won't Believe" "Secret" "Guess" "Top") }
+  def validateCB
+    ["Won't Believe", "Secret", "Guess", "Top"].each do |t|
+      if  self.title != nil && self.title.include?(t)
+        return true
+      end
+    end
+    errors.add(:title, "Error!")
+  end
 end
